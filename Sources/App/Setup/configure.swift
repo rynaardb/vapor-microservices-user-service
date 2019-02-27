e76fd84e76214a4c1f2ee22de8712ba615fe5ee3
Rynaard Burger <ryno.burger@gmail.com>
2019-02-27 22:30:54 +0100

    Completed initial implementation

--- Sources/App/Setup/configure.swift
+++ Sources/App/Setup/configure.swift
@@ -15,8 +15,44 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewaresConfig)
     
     var databasesConfig = DatabasesConfig()
-    try databases(config: &databasesConfig)
-    services.register(databasesConfig)
+    let databaseConfig: PostgreSQLDatabaseConfig
+
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else if let url = Environment.get("DB_POSTGRESQL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else {
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        let databaseName: String
+        let databasePort: Int
+        if (env == .testing) {
+            databaseName = "vapor-test"
+            if let testPort = Environment.get("DATABASE_PORT") {
+                databasePort = Int(testPort) ?? 5433
+            } else {
+                databasePort = 5433
+            }
+        } else {
+            databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+            databasePort = 5432
+        }
+
+        databaseConfig = PostgreSQLDatabaseConfig(
+            hostname: hostname,
+            port: databasePort,
+            username: username,
+            database: databaseName,
+            password: password)
+    }
+
+    let database = PostgreSQLDatabase(config: databaseConfig)
+
+    databasesConfig.add(database: database, as: .psql)
+
+    //try databases(config: &databasesConfig)
+    //services.register(databasesConfig)
 
     services.register { container -> MigrationConfig in
         var migrationConfig = MigrationConfig()
